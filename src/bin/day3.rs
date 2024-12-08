907dc15b84d65ce30ced0d7f4d1da9ff98c3dcda
Frederik Finckh <frederik.finckh@gmx.de>
2024-12-08 15:41:22 +0100

    day3 part two

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -2,11 +2,11 @@ use std::fs::read_to_string;
 
 fn main() {
     let input = read_to_string("inputs/day3").unwrap().to_string();
-    p1(&input);
-    p2(&input);
+    println!("{}", p1(&input));
+    println!("{}", p2(&input));
 }
 
-fn p1(input: &String) {
+fn p1(input: &String) -> u64 {
     let result: u64 = input
         .split("mul(")
         .map(|mul_candidate: &str| parse_mul(mul_candidate))
@@ -14,7 +14,8 @@ fn p1(input: &String) {
         .map(|x| x.unwrap())
         .map(|(a, b)| a * b)
         .sum();
-    println!("{}", result);
+
+    result
 }
 
 fn parse_mul(mul_candidate: &str) -> Option<(u64, u64)> {
@@ -34,6 +35,18 @@ fn parse_mul(mul_candidate: &str) -> Option<(u64, u64)> {
     return_val
 }
 
-fn p2(input: &String) {
-    println!("{}", input);
+fn p2(input: &String) -> u64 {
+    let mut manipulated_input = "do()".to_string();
+    manipulated_input.push_str(input);
+
+    manipulated_input
+        .split("don't()")
+        .map(|dont_block| {
+            dont_block
+                .split("do()")
+                .skip(1)
+                .map(|do_block| p1(&do_block.to_string()))
+                .sum::<u64>()
+        })
+        .sum()
 }
