d1d64d4d21e59efc0b858e5d1bf97d74fc94d0eb
Frederik Finckh <frederik.finckh@gmx.de>
2024-12-08 14:25:02 +0100

    day2 solved

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -1,7 +1,7 @@
 use std::fs::read_to_string;
 
 fn main() {
-    let input: Vec<Vec<i64>> = read_to_string("inputs/day2_test")
+    let input: Vec<Vec<i64>> = read_to_string("inputs/day2")
         .unwrap()
         .split('\n')
         .filter(|l| l.len() > 0)
@@ -21,28 +21,39 @@ fn p1(input: &Vec<Vec<i64>>) {
 }
 
 fn is_safe(levels: &Vec<i64>) -> bool {
+    is_safe_increasing(levels) || is_safe_decreasing(levels)
+}
+
+fn is_safe_increasing(levels: &Vec<i64>) -> bool {
     if let Some(mut prev) = levels.get(0) {
-        let first = prev.clone();
-        //check safe increasing
-        let mut safe_increasing = true;
         for level in levels.iter().skip(1) {
-            if level <= prev || *level > prev + 3 {
-                safe_increasing = false;
-                break;
+            if !is_safe_increase(level, prev) {
+                return false;
             }
             prev = level;
         }
-        if !safe_increasing {
-            prev = &first;
-            for level in levels.iter().skip(1) {
-                if level >= prev || *level < prev - 3 {
-                    return false;
-                }
-                prev = level;
+    }
+    true
+}
+
+fn is_safe_increase(level: &i64, prev: &i64) -> bool {
+    level > prev && *level <= prev + 3
+}
+
+fn is_safe_decreasing(levels: &Vec<i64>) -> bool {
+    if let Some(mut prev) = levels.get(0) {
+        for level in levels.iter().skip(1) {
+            if !is_safe_decrese(level, prev) {
+                return false;
             }
+            prev = level;
         }
     }
     true
+}
+
+fn is_safe_decrese(level: &i64, prev: &i64) -> bool {
+    level < prev && *level >= prev - 3
 }
 
 fn p2(input: Vec<Vec<i64>>) {
@@ -54,89 +65,36 @@ fn p2(input: Vec<Vec<i64>>) {
 }
 
 fn is_safe_with_dampening(levels: &Vec<i64>) -> bool {
-    println!();
-    println!();
-    println!();
-    println!("=====");
-    if let Some(mut prev_prev) = levels.get(0) {
-        if let Some(mut prev) = levels.get(1) {
-            println!("checking {:?} for increasing", levels);
-            let second = prev.clone();
-            let first = prev_prev.clone();
-            let mut dampener_used = false;
+    if let Some(_first) = levels.get(0) {
+        let mut safe_increasing = true;
 
-            let mut safe_increasing = true;
+        for index in 1..levels.len() {
+            if !is_safe_increase(&levels[index], &levels[index - 1]) {
+                let mut levels_without_current = levels[..index].to_vec();
+                levels_without_current.append(&mut levels[index + 1..].to_vec());
 
-            for level in levels.iter().skip(2) {
-                if level <= prev || *level > prev + 3 {
-                    if dampener_used {
-                        safe_increasing = false;
-                        break;
-                    } else {
-                        println!("using dampener, {:?}", levels);
-                        dampener_used = true;
-                        // if skipping prev is ok continue with prev_prev=prev_prev, level=prev
-                        if level > prev_prev && *level <= prev_prev + 3 {
-                            //skip prev
-                            println!(
-                                "skipping prev {} at {}, {}, {}",
-                                prev, prev_prev, prev, level
-                            );
-                            prev = level;
-                        } else {
-                            //skip level
-                            println!(
-                                "skipping level {} at {}, {}, {}",
-                                level, prev_prev, prev, level
-                            );
-                            continue;
-                        }
-                    }
-                } else {
-                    prev_prev = prev;
-                    prev = level;
-                }
+                let mut levels_without_prev = levels[..index - 1].to_vec();
+                levels_without_prev.append(&mut levels[index..].to_vec());
+
+                safe_increasing = is_safe_increasing(&levels_without_current)
+                    || is_safe_increasing(&levels_without_prev);
+                break;
             }
+        }
+        if !safe_increasing {
+            for index in 1..levels.len() {
+                if !is_safe_decrese(&levels[index], &levels[index - 1]) {
+                    let mut levels_without_current = levels[..index].to_vec();
+                    levels_without_current.append(&mut levels[index + 1..].to_vec());
 
-            if !safe_increasing {
-                println!("checking {:?} for decreasing", levels);
-                prev_prev = &first;
-                prev = &second;
-                dampener_used = false;
-                for level in levels.iter().skip(2) {
-                    if level >= prev || *level < prev - 3 {
-                        if dampener_used {
-                            return false;
-                        } else {
-                            println!("using dampener, {:?}", levels);
-                            dampener_used = true;
-                            // if skipping prev is ok continue with prev_prev=prev_prev, level=prev
-                            // otherwise skip level
-                            if level < prev_prev && *level >= prev_prev - 3 {
-                                println!(
-                                    "skipping prev {} at {}, {}, {}",
-                                    prev, prev_prev, prev, level
-                                );
-                                // skip prev
-                                prev = level;
-                            } else {
-                                println!(
-                                    "skipping level {} at {}, {}, {}",
-                                    level, prev_prev, prev, level
-                                );
-                                // skip level
-                                continue;
-                            }
-                        }
-                    } else {
-                        prev_prev = prev;
-                        prev = level;
-                    }
+                    let mut levels_without_prev = levels[..index - 1].to_vec();
+                    levels_without_prev.append(&mut levels[index..].to_vec());
+
+                    return is_safe_decreasing(&levels_without_current)
+                        || is_safe_decreasing(&levels_without_prev);
                 }
             }
         }
     }
-
-    println!("safe!");
     true
 }
