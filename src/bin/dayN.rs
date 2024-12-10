1649c753b7797ca6712036a92c2dcbef50599cb2
Frederik Finckh <frederik.finckh@gmx.de>
2024-12-10 22:33:27 +0100

    adapt template

--- src/bin/dayN.rs
+++ src/bin/dayN.rs
@@ -1,15 +1,23 @@
 use std::fs::read_to_string;
 
 fn main() {
-    let input = read_to_string("inputs/day3_test").unwrap().to_string();
-    p1(&input);
-    p2(&input);
+    let input: Vec<String> = read_to_string("inputs/dayN_test")
+        .unwrap()
+        .to_string()
+        .split('\n')
+        .filter(|x| x.len() > 0)
+        .map(|x| x.to_string())
+        .collect();
+    println!("{}", p1(&input));
+    println!("{}", p2(&input));
 }
 
-fn p1(input: &String) {
-    println!("{}", input);
+fn p1(input: &Vec<String>) -> u64 {
+    println!("{:?}", input);
+    1
 }
 
-fn p2(input: &String) {
-    println!("{}", input);
+fn p2(input: &Vec<String>) -> u64 {
+    println!("{:?}", input);
+    2
 }
