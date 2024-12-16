e68cceb26cff330a6aa1fa372b9eb9baa6cee5ab
Frederik Finckh <frederik.finckh@gmx.de>
2024-12-16 23:22:24 +0100

    day14 logic and vizualization

--- src/bin/day14.rs
+++ src/bin/day14.rs
@@ -46,12 +46,54 @@ fn main() {
     println!("{}", p2(&input, field_size));
 }
 
-fn p1(input: &Vec<((i64, i64), (i64, i64))>, field_size: (isize, isize)) -> u64 {
-    println!("{:?}, {:?}", input, field_size);
+fn p1(input: &Vec<((i64, i64), (i64, i64))>, field_size: (i64, i64)) -> u64 {
+    let seconds = 100;
+    let positions_after_seconds = update_positions(input, field_size, seconds);
+    println!("{:?}", positions_after_seconds);
+    visualize(field_size, positions_after_seconds);
     1
 }
 
-fn p2(input: &Vec<((i64, i64), (i64, i64))>, field_size: (isize, isize)) -> u64 {
+fn update_positions(
+    input: &Vec<((i64, i64), (i64, i64))>,
+    field_size: (i64, i64),
+    seconds: i64,
+) -> Vec<(i64, i64)> {
+    let positions_after_seconds: Vec<(i64, i64)> = input
+        .iter()
+        .map(|(position, velocity)| {
+            (
+                (position.0 + seconds * velocity.0)
+                    - field_size.0 * ((position.0 + seconds * velocity.0) / field_size.0),
+                (position.1 + seconds * velocity.1)
+                    - field_size.1 * ((position.1 + seconds * velocity.1) / field_size.1),
+            )
+        })
+        .map(|(x, y)| {
+            (
+                if x >= 0 { x } else { x + field_size.0 },
+                if y >= 0 { y } else { y + field_size.1 },
+            )
+        })
+        .collect();
+    positions_after_seconds
+}
+
+fn visualize(field_size: (i64, i64), positions_after_100s: Vec<(i64, i64)>) {
+    println!();
+    for y in 0..field_size.1 {
+        for x in 0..field_size.0 {
+            if positions_after_100s.contains(&(x, y)) {
+                print!("X");
+            } else {
+                print!(".");
+            }
+        }
+        println!();
+    }
+}
+
+fn p2(input: &Vec<((i64, i64), (i64, i64))>, field_size: (i64, i64)) -> u64 {
     println!("{:?}, {:?}", input, field_size);
     2
 }
